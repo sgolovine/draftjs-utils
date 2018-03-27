03e946f1707b5fffa2241e4d7a84a042a9f1053c
sgolovine <sunny@glvn.co>
2018-03-27 10:54:58 -0400

    Added sidebar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import React, { Component } from 'react'
-
+import React, { Component } from "react"
+import AppSidebar from "./Sidebar"
 class App extends Component {
   render() {
     return (
-      <p>App.js</p>
-    );
+      <AppSidebar>
+        <p>App.js</p>
+      </AppSidebar>
+    )
   }
 }
 
-export default App;
+export default App
---
+++ src/Sidebar.js
@@ -0,0 +1,76 @@
+import React, { Component } from "react"
+import PropTypes from "prop-types"
+import Sidebar from "react-sidebar"
+
+const sidebarStyles = {
+  root: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    overflow: "hidden",
+  },
+  sidebar: {
+    zIndex: 2,
+    position: "absolute",
+    top: 0,
+    bottom: 0,
+    transition: "transform .3s ease-out",
+    WebkitTransition: "-webkit-transform .3s ease-out",
+    willChange: "transform",
+    overflowY: "auto",
+    width: "15em",
+  },
+  content: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    overflowY: "scroll",
+    WebkitOverflowScrolling: "touch",
+    transition: "left .3s ease-out, right .3s ease-out",
+    padding: "16px",
+  },
+  overlay: {
+    zIndex: 1,
+    position: "fixed",
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    opacity: 0,
+    visibility: "hidden",
+    transition: "opacity .3s ease-out, visibility .3s ease-out",
+    backgroundColor: "rgba(0,0,0,.3)",
+  },
+  dragHandle: {
+    zIndex: 1,
+    position: "fixed",
+    top: 0,
+    bottom: 0,
+  },
+}
+class AppSidebar extends Component {
+  renderSidebarContent() {
+    return <p>Sidebar Content</p>
+  }
+  render() {
+    return (
+      <Sidebar
+        sidebar={this.renderSidebarContent()}
+        docked={true}
+        styles={sidebarStyles}
+      >
+        {this.props.children}
+      </Sidebar>
+    )
+  }
+}
+
+AppSidebar.propTypes = {
+  children: PropTypes.any,
+}
+
+export default AppSidebar
